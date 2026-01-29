9463d69a815301b603becd4c970f411b705d15b1
Juan Sebastian Martínez <juansemar123456789@gmail.com>
2026-01-29 11:03:36 -0300

    FIX endpoint include_Movimientos cta cte

--- src/modules/cc/estado-cuenta.controller.ts
+++ src/modules/cc/estado-cuenta.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, Req } from '@nestjs/common';
+import type { Request } from 'express';
 import { QueryEstadoCuentaDto } from './dto/query-estado-cuenta.dto';
 import { EstadoCuentaService } from './estado-cuenta.service';
 
@@ -7,7 +8,9 @@ export class EstadoCuentaController {
   constructor(private readonly service: EstadoCuentaService) {}
 
   @Get()
-  async get(@Query() q: QueryEstadoCuentaDto) {
-    return this.service.estadoCuenta(q);
+  async get(@Query() q: QueryEstadoCuentaDto, @Req() req: Request) {
+    // ✅ lee el valor REAL que llegó por querystring
+    const includeMovsRaw = (req.query as any)?.include_movimientos;
+    return this.service.estadoCuenta(q, includeMovsRaw);
   }
 }
